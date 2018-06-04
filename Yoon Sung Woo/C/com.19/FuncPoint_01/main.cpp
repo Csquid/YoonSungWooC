927adbccaf08533cbe859fe63bcf895bbab8badd
small _Coin <rmaqlctnekf@naver.com>
2018-06-04 16:26:09 +0900

    C __ FunctionPointer

---
+++ Yoon Sung Woo/C/com.19/FuncPoint_01/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void SimpleAdder(int n1, int n2)
+{
+    printf("%d + %d = %d \n", n1, n2, n1 + n2);
+}
+
+void ShowString(const char* str)
+{
+    printf("%s \n", str);
+}
+
+int main()
+{
+    const char* str = "Function Pointer";
+    int num1  = 10;
+    int num2  = 20;
+
+    void (*fptr1)(int, int)       = SimpleAdder;
+    void (*fptr2)(const char *)   = ShowString;
+
+    fptr1(num1, num2);
+    fptr2(str);
+
+    printf("fptr1 address: %p \n", fptr1);
+    printf("fptr2 address: %p \n\n", fptr2);
+
+    printf("SimpleAdder address: %p \n", SimpleAdder);
+    printf("ShowString address: %p", ShowString);
+    return 0;
+}
